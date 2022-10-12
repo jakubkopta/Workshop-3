ac400d902ebd6a0f6534838b0a73d6700b467fda
Jakub Kopta <jakubkopta@gmail.com>
2022-10-12 16:31:29 +0200

    HTML file

--- src/main/java/pl/coderslab/users/UserList.java
+++ src/main/java/pl/coderslab/users/UserList.java
@@ -1,5 +1,7 @@
 package pl.coderslab.users;
 
+import pl.coderslab.UserDao;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,8 +12,10 @@ import java.io.IOException;
 @WebServlet("/user/list")
 public class UserList extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/users/list.jsp")
-                .forward(request, response);
+        getServletContext().getRequestDispatcher("/users/list.jsp").forward(request, response);
+
+        UserDao userDao = new UserDao();
+        request.setAttribute("users", userDao.findAll());
     }
 }
 
